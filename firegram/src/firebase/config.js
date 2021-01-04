ba71b2e423399a18bcf4e371161e0b83032430a6
Joe Bentley <joe.bentley.20@gmail.com>
2021-01-04 23:17:56 +0000

    Setup Firebase

---
+++ firegram/src/firebase/config.js
@@ -0,0 +1,20 @@
+ import * as firebase from 'firebase/app';
+ import 'firebase/storage';
+ import 'firebase/firestore';
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBV05iu0DzwEmIcZlncAifweAdqMlMjU0s",
+    authDomain: "joe-bentley-firegram.firebaseapp.com",
+    projectId: "joe-bentley-firegram",
+    storageBucket: "joe-bentley-firegram.appspot.com",
+    messagingSenderId: "671107552048",
+    appId: "1:671107552048:web:1ea2daaa9fa51d7a572ef5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export { projectStorage, projectFirestore };
